6faa2a9035ce0617df03d2c712e095745743c209
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-20 11:53:59 -0300

    add spring data application

---
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -0,0 +1,47 @@
+package br.com.alura.spring.data;
+
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
+
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+
+	private Boolean system = true;
+
+	private final CrudCargoService crudCargoService;
+
+	public SpringDataApplication(CrudCargoService crudCargoService) {
+		this.crudCargoService = crudCargoService;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		while(system) {
+			System.out.println("Qual acao você quer executar");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+
+			if (action == 1) {
+				crudCargoService.inicial(scanner);
+			} else {
+				system = false;
+			}
+		}
+	}
+
+}
