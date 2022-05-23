470be5d6d484a59cb9b057815ecf7cd4acd99790
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-23 13:58:58 -0300

    add funcionario class

---
+++ src/main/java/br/com/alura/spring/data/orm/Funcionario.java
@@ -0,0 +1,106 @@
+package br.com.alura.spring.data.orm;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String nome;
+  private String cpf;
+  private BigDecimal salario;
+  private LocalDate dataContratacao = LocalDate.now();
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  private Cargo cargo;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "funcionarios_unidades", joinColumns = {@JoinColumn(name = "fk_funcionario")}, inverseJoinColumns = { @JoinColumn(name = "fk_unidade") })
+  private List<UnidadeTrabalho> unidadeTrabalhos = new ArrayList<>();
+
+  public Integer getId() {
+    return id;
+  }
+  
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public BigDecimal getSalario() {
+    return salario;
+  }
+
+  public void setSalario(BigDecimal salario) {
+    this.salario = salario;
+  }
+
+  public LocalDate getDataContratacao() {
+    return dataContratacao;
+  }
+
+  public void setDataContratacao(LocalDate dataContratacao) {
+    this.dataContratacao = dataContratacao;
+  }
+
+  public Cargo getCargo() {
+    return cargo;
+  }
+
+  public void setCargo(Cargo cargo) {
+    this.cargo = cargo;
+  }
+
+  public List<UnidadeTrabalho> getUnidadeTrabalhos() {
+    return unidadeTrabalhos;
+  }
+
+  public void setUnidadeTrabalhos(List<UnidadeTrabalho> unidadeTrabalhos) {
+    this.unidadeTrabalhos = unidadeTrabalhos;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("Funcionario [id=" + id + ", nome=" + nome + ", salario=" + salario + ", dataContratacao=" + dataContratacao + ", cargo=" + cargo + "] \n Unidades ");
+
+    unidadeTrabalhos.forEach(unidadeTrabalho -> {
+      builder.append(unidadeTrabalho.toString());
+      builder.append(" ");
+    });
+
+    return builder.toString();
+  }
+}
