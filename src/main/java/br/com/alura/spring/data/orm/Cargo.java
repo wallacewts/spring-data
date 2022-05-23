78a35bd99f49ce18ee53542f690330189aa2786f
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-23 13:58:45 -0300

    override to string method

--- src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -29,4 +29,9 @@ public class Cargo {
   public void setDescricao(String descricao) {
     this.descricao = descricao;
   }
+
+  @Override
+  public String toString() {
+    return "Cargo [descricao=" + descricao + ", id=" + id + "]";
+  }
 }
