2ad2fc70a97f17d474ddad392a0f7eb312014b6d
Matheus Wallace <matheuswts2011@hotmail.com>
2022-05-23 14:53:03 -0300

    create spring specficiation for funcionario

---
+++ src/main/java/br/com/alura/spring/data/specification/SpecificationFuncionario.java
@@ -0,0 +1,26 @@
+package br.com.alura.spring.data.specification;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.com.alura.spring.data.orm.Funcionario;
+
+public class SpecificationFuncionario {
+  public static Specification<Funcionario> nome(String nome) {
+    return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+  }
+
+  public static Specification<Funcionario> cpf(String cpf) {
+    return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("cpf"), cpf);
+  }
+
+  public static Specification<Funcionario> salario(BigDecimal salario) {
+    return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("salario"), salario);
+  }
+
+  public static Specification<Funcionario> dataContratacao(LocalDate dataContratacao) {
+    return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThan(root.get("dataContratacao"), dataContratacao);
+  }
+}
